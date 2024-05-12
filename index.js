447f6637678f73e54c59d1a4e062d7dde57cefae
Gopikrishnan Rajeev <gopikrishnan.rmg@live.in>
2024-05-12 01:26:05 -0400

    Added animations to be played when elements comes into view

--- index.js
+++ index.js
@@ -7,3 +7,17 @@ setInterval(() => {
   currentWord = (currentWord + 1) % words.length;
   target.textContent = `${words[currentWord]}`;
 }, 2000);
+
+
+const inViewport = (entries, observer) => {
+  entries.forEach(entry => {
+      entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+  });
+};
+
+const Obs = new IntersectionObserver(inViewport);
+const obsOptions = {}; 
+
+document.querySelectorAll("progress").forEach(el => {
+    Obs.observe(el, obsOptions);
+});
